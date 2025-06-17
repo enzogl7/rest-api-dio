41bb0e98fc1b8ff0b0d158afa3824413572e47c3
enzogl7 <enzolima527@gmail.com>
2025-06-17 17:46:29 -0300

    create project

---
+++ src/main/java/com/example/demo/RestApiDioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDioApplication.class, args);
+	}
+
+}
